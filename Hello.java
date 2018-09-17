15e4fdce55db9cd2fb86ca6fa882bbc90dd5d504
sbrink00 <sbrink00@stuy.edu>
2018-09-16 20:46:15 -0400

    I substituted the greeting Hello world with my own greeting and I changed the class from Hello World to Hello in order to make the file run correctly

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class HelloWorld{
+public class Hello{
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("Hello my name is Sam, how are you?");
     }
 }
